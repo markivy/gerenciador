a975d2339b569aa1ff8de3338ea7a7c833f6d4f3
markivy <marcoshgpereira@hotmail.com>
2024-01-24 00:30:44 -0300

    New pages and connected also.

--- node/app.js
+++ node/app.js
@@ -3,3 +3,25 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/home/home.html'));
+});
+
+app.get('/vendas',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/vendas/vendas.html'));
+});
+
+
+app.get('/caixa',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/caixa/caixa.html'));
+});
+
+app.get('/estoque',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/estoque/estoque.html'));
+});
